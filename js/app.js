de5488f78540c3a029a4f64ed3c707a45727a4d7
ssisk <stephen@stephensisk.net>
2013-05-30 09:22:08 -0700

    Add some wiring to the picker page
    There's some commented out stuff for the card picking algorithm as well.

--- js/app.js
+++ js/app.js
@@ -6,9 +6,15 @@ App.Router.map(function() {
     this.resource('cards');
 });
 
+/*App.IndexRoute = Ember.Route.Extend({
+    redirect: function() {
+        this.transitionTo('picker');
+    }
+});*/
+
 App.CardsRoute = Ember.Route.extend({
     model: function() {
-	return App.Card.find();
+    	return App.Card.find();
     }
 })
 
@@ -21,12 +27,43 @@ App.Card = DS.Model.extend({
 //    set: DS.attr('number')
 });
 
+App.PickerController = Ember.ObjectController.extend({
+    generate: function() {
+        this.set('cardsToDisplay', App.Card.find());
+        this.set('weHaveCards', true);
+    },  
+    cardsToDisplay: [],
+    weHaveCards: false
+});
+
 App.Card.FIXTURES = [
     // Base
-    {id: 1, name: 'Library'},
+    {id: 100, name: 'Library'},
     // Seaside
-    {id: 2, name: 'Pirate Ship'},
+    {id: 200, name: 'Pirate Ship'},
     // Intrigue
-    {id: 3, name: 'Baron'},
+    {id: 300, name: 'Baron'},
+    {id: 301, name: 'Masquerade'},
 
 ];
+
+/* pickCards will be some sort of strategy pattern */
+/*
+
+    options looks something like: 
+    { 
+        noAttack: true,
+    }
+
+    I think there are two types of card filters:
+    * filters that operate on only one card ("No attack cards")
+    * filters that operate on the set of cards ("no unbalanced attack cards")
+
+    the one card filters could probably be run on allCards at the beginning,
+    but the multiple card ones will probably need to adopt a "generate 10, 
+    check for compliance, then mutate if necessary" strategy
+*/
+/*
+App.pickCards = function (options, allCards) {
+    return allCards;
+};*/
