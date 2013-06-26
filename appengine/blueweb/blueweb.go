e0abd72a70ed15f6bc6222273b3549780d66b07f
ssisk <stephen@stephensisk.net>
2013-06-26 09:00:50 -0700

    Use crypto/rand to generate random cards

--- appengine/blueweb/blueweb.go
+++ appengine/blueweb/blueweb.go
@@ -3,9 +3,11 @@ package blueweb
 import (
   "appengine"
   "appengine/datastore"
+  "crypto/rand"
   "encoding/json"
   "fmt"
   "io/ioutil"
+  "math/big"
   "net/http"
 )
 
@@ -32,9 +34,62 @@ func readStructFromJSONRequest(w http.ResponseWriter, r *http.Request, readInto 
   return json.Unmarshal(jsonRaw, readInto)
 }
 
+
+func generateRandom(options *Get10Options, c chan int) {
+/*
+  The general strategy here is to generate a list with all the cards in it,
+  then randomly pick indexes out of - we then mark that item in the list
+  as used. If we happen to pick an already used card, we will just keep
+  picking until we pick an unused card. Since the number of cards needed
+  should be significantly smaller than the size of the deck, collisions shouldn't
+  be an issue.
+
+  I think that will end up being something like O(cards_needed), so
+  somewhat close to 10.
+
+  We could have generated the deck, then sorted it, but we think that the
+  deck size will always be at least 2x number of cards needed. sorting the deck is guaranteed 
+  to be O(nlogn), which I expect to be much larger than O(cards_needed), but the pathological
+  case is an infinite loop. We think the pathological case won't happen that often,
+  this is good enough for now.
+*/
+
+
+  // todo: use the actual set of cards, and pull things out based on the options
+  cards := []int{1, 2, 3, 7, 8, 6, 5, 20, 18, 19, 14, 12, 13}
+
+//  curLen := new(big.Int)
+//  curLen.SetInt(&)
+  curLen := big.NewInt(int64(len(cards)))
+  for numGenerated := 0; numGenerated < len(cards); numGenerated += 1 {
+    curBigCard,_ := rand.Int(rand.Reader, curLen)
+    curCard := curBigCard.Int64()
+    for cards[curCard] == 0 {  
+      curBigCard, _ = rand.Int(rand.Reader, curLen)
+      curCard = curBigCard.Int64()
+    }
+    c <- cards[curCard]
+    cards[curCard] = 0
+    // todo: make it so we don't die in the pathological case
+    // one idea is that when numGenerated 
+  }
+
+  close (c)
+}
+
 // this is where all the magic happens for generating the 10 cards
 func generateCards(options *Get10Options) []int {
-  return []int{1, 2, 3, 7, 9, 4, 0, 8, 6, 5}
+
+  c := make(chan int)
+  go generateRandom(options, c)
+  
+  cards := make([]int, 10)
+
+  for i := 0; i < 10; i++ {
+    cards[i] = <- c
+  }
+
+  return cards
 }
 
 func generateGet10Response(cards []int, w http.ResponseWriter) {
